1f11b291dd648329302126c7bea2f7cf483d0f33
AntonKAb <anton_10_1999@mail.ru>
2019-05-18 15:42:16 +0700

    version 6.

--- main.py
+++ main.py
@@ -6,25 +6,20 @@ games.init(screen_width=1024, screen_height=512, fps=60)
 
 
 class Hero(games.Sprite):
-    first = 1
-    second = 2
 
-    dir = 1
-    image = [0, 0]
-    image[0] = games.load_image('img/hero_11.png')
-    image[1] = pygame.transform.flip(image[0], 1, 0)
+    points = 0
     sound = games.load_sound('sounds/game_over.wav')
-
+    scr = games.load_image('img/hero_11.png')
     reloading = 0
     pause = 70
 
     def __init__(self, dx):
-        super(Hero, self).__init__(image=Hero.image[0],
+        super(Hero, self).__init__(image=Hero.scr,
                                    x=games.screen.width / 2,
                                    bottom=games.screen.height - 90, dx=dx)
         self.dx = dx
 
-        self.score = games.Text(value=0,
+        self.score = games.Text(value=Hero.points,
                                 size=32,
                                 color=colour.black,
                                 top=5,
@@ -35,7 +30,7 @@ class Hero(games.Sprite):
         games.screen.add(self.score)
         games.screen.add(self.score_letter)
 
-        self.health = games.Text(value=2,
+        self.health = games.Text(value=1,
                                  size=32,
                                  color=colour.black,
                                  top=30, right=games.screen.width - 12)
@@ -47,11 +42,6 @@ class Hero(games.Sprite):
     def update(self):
         if self.reloading > 0:
             self.reloading -= 1
-        if games.keyboard.is_pressed(games.K_q):
-            Hero.dir = -1
-        if games.keyboard.is_pressed(games.K_e):
-            Hero.dir = 1
-
         if games.keyboard.is_pressed(games.K_a):
             self.dx = -1
         if games.keyboard.is_pressed(games.K_d):
@@ -62,6 +52,13 @@ class Hero(games.Sprite):
         if self.right > games.screen.width:
             self.right = games.screen.width
 
+        if games.keyboard.is_pressed(games.K_1) and self.reloading == 0:
+            knife = Knife(self.x)
+            games.screen.add(knife)
+            self.reloading = Hero.pause
+            if self.left < 0:
+                knife.destroy()
+
         if games.keyboard.is_pressed(games.K_SPACE) and self.reloading == 0:
             one_shoot = Bullet(self.x)
             games.screen.add(one_shoot)
@@ -70,25 +67,23 @@ class Hero(games.Sprite):
                 one_shoot.destroy()
             if self.left < 0:
                 one_shoot.destroy()
+
         for _ in self.overlapping_sprites:
-            if not games.keyboard.is_pressed(games.K_SPACE):
+            if not games.keyboard.is_pressed(games.K_SPACE) and not games.keyboard.is_pressed(games.K_1):
                 for sprite in self.overlapping_sprites:
                     sprite.destroy()
                     self.health.value -= 1
+            if games.keyboard.is_pressed(games.K_SPACE) or games.keyboard.is_pressed(games.K_1):
+                    self.score.value += 1
         if self.health.value == 0:
             self.endgame()
         if self.health.value <= 0:
             self.endgame()
-        self.next_direction()
 
-    def next_direction(self):
-        if Hero.dir == -1:
-            self.image = Hero.image[1]
-        else:
-            self.image = Hero.image[0]
-    #     self.direction *= -1
-    #     depend = {-1: 1, 1: 0}
-    #     self.image = Hero.image[depend[self.direction]]
+
+    def check_score(self):
+        if self.overlapping_sprites:
+            self.score += 1
 
 
     @staticmethod
@@ -101,18 +96,19 @@ class Hero(games.Sprite):
 
 
 class Ghost(games.Sprite):
-    left_border = -50
-    right_border = games.screen.width
+    left_border = random.choice([-50, -70])
+    right_border = random.choice([games.screen.width + 20, games.screen.width])
     run = 1
-    img = {'type_one': games.load_image('img/ghost_1.png'), 'type_two': games.load_image('img/ghost_2.png')}
+    img = {'type_one': games.load_image('img/ghost_1.png'),
+           'type_two': games.load_image('img/ghost_2.png'),
+           'type_three': games.load_image('img/ghost_2.png'),
+           'type_four': games.load_image('img/ghost_2.png')}
 
     def __init__(self):
-        super(Ghost, self).__init__(image=Ghost.img[random.choice(['type_one', 'type_two'])],
+        super(Ghost, self).__init__(image=Ghost.img[random.choice(['type_one', 'type_two',
+                                                                   'type_three', 'type_four'])],
                                     x=random.choice([self.left_border, self.right_border]),
                                     bottom=games.screen.height - 90, dx=0)
-
-    def death(self):
-        self.destroy()
 
     def update(self):
         if self.x == self.left_border:
@@ -132,19 +128,26 @@ class Bullet(games.Sprite):
         Bullet.sound.play()
 
     def update(self):
-        if Hero.dir == 1:
-            self.dx = 15
             for _ in self.overlapping_sprites:
                 if not games.keyboard.is_pressed(games.K_SPACE):
                     for sprite in self.overlapping_sprites:
                         sprite.destroy()
-        else:
-            self.dx = -15
-            self.x *= -1
-            for _ in self.overlapping_sprites:
-                if not games.keyboard.is_pressed(games.K_SPACE):
-                    for sprite in self.overlapping_sprites:
-                        sprite.destroy()
+                    Hero.points += 1
+
+
+class Knife(games.Sprite):
+    image = games.load_image('img/knife.png')
+    speed = 4
+
+    def __init__(self, x):
+        super(Knife, self).__init__(image=Knife.image, x=x - 89, dx=Knife.speed * -1, bottom=games.screen.height - 140)
+
+    def update(self):
+        for _ in self.overlapping_sprites:
+            if not games.keyboard.is_pressed(games.K_1):
+                for sprite in self.overlapping_sprites:
+                    sprite.destroy()
+                Hero.points += 1
 
 
 class Move_Right(games.Animation):
@@ -168,7 +171,6 @@ class Ghost_Death:
 
 
 class Start:
-    _list = []
 
     def __init__(self):
 
@@ -182,20 +184,18 @@ class Start:
         wall_image = games.load_image('img/wall_1.jpg', transparent=False)
         games.screen.background = wall_image
 
-    def new_ghost(self):
-        ghost = Ghost()
-        games.screen.add(ghost)
-
 
 def main():
     play_game = Start()
     play_game.begin()
 
-    for _ in range(1, 2):
-        play_game.new_ghost()
+    all_en = []
 
-    ghost = Ghost()
-    games.screen.add(ghost)
+    enemy = [Ghost() for _ in range(3)]
+    for obj in enemy:
+        all_en.append(obj)
+    for el in all_en:
+        games.screen.add(el)
 
     games.mouse.is_visible = False
     games.screen.mainloop()
